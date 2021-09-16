5ab4bf094b0c07c68ad07a0228f4e4bfdf9efe38
Jorge Montejo <jorgemon.lopez@gmail.com>
2021-09-16 17:31:14 +0200

    feat: find summoner by id

--- src/league/summoner.js
+++ src/league/summoner.js
@@ -29,17 +29,29 @@ async function summonerRequest(region, path, summoner) {
  */
 async function getSummonerByName(summonerName, region = "") {
 
-    if ((region && !Object.values(regions).includes(region)) || (!region && !Object.values(regions).includes(options.defaultRegion))) {
-        throw new Error("Region: '" + region + "' is not a valid region");
-    }
-
     if (!summonerName) {
         throw new Error("Summoner name not found");
     }
 
-    const url = (region ? region : options.defaultRegion) + endpoints.base + endpoints.lol.summoner + "by-name/" + summonerName;
+    return await summonerRequest(region, "by-name/", summonerName);
+}
+
+/**
+ * Get a summoner by their id
+ * @param {String} encryptedSummonerId 
+ * @param {Region} [region] 
+ * @returns {{ data: Object, http_code: Number, error_msg: (String|undefined) }} response
+ */
+async function getSummonerById(summonerId, region = "") {
+
+    if (!summonerId) {
+        throw new Error("Summoner name not found");
+    }
+
+    return await summonerRequest(region, "", summonerId);
 }
 
 module.exports = {
-    getSummonerByName
+    getSummonerByName,
+    getSummonerById
 }
